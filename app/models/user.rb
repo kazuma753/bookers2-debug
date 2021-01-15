8aa3846fc6eda8ac8a58873131912d4313dd8faf
vagrant <vagrant@localhost.localdomain>
2021-01-15 13:35:47 +0000

    1/15

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   attachment :profile_image
   has_many :books, dependent: :destroy
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
